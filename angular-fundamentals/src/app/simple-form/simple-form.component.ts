c05ce201bfefee6488b0a4f385e6a8a03afb2649
XXB <xxblcw@163.com>
2018-06-09 12:33:45 +0800

    获取鼠标、键盘事件

--- angular-fundamentals/src/app/simple-form/simple-form.component.ts
+++ angular-fundamentals/src/app/simple-form/simple-form.component.ts
@@ -15,17 +15,64 @@
 
 import { Component, OnInit } from '@angular/core';
 
+/***
+ * 
+ * 通过#variableName 语法，我们获取的对象是对应 DOM 元素的引用
+ * 
+ * 引入 $event 变量获取正确的鼠标事件。
+ * (click)="onClick(myInput.value, $event)"
+ * 
+ * 当 Angular 在调用我们的事件处理函数时，会自动帮我们处理调用的参数。$event 自动映射为触发的事件。
+ * 
+ * 除了监听鼠标事件外，我们还可以监听键盘事件。
+ * (keydown.enter)="onEnter($event, myInput.value)" 表达式表示我们监听键盘 enter 键的按下事件，按下键盘的 enter 键时，将会调用组件类中定义的 onEnter() 方法。
+ * 
+ * 同样也可以通过 $event 来获取 KeyboardEvent 对象。
+ * 
+ * ***/
+
 @Component({
   selector: 'app-simple-form',
   // templateUrl: './simple-form.component.html',
   template: `
-    <p>
-      simple-form Works!
-    </p>
+    <div>
+      <input #myInput type="text">
+      <div>
+        <button (click)="onClick(myInput.value)">获取输入框内的值</button>
+        <!-- 输出输入框内的值 -->
+        <span>{{myInput.value}}</span>
+      </div>
+      <br>
+      <div>
+        <button (click)="onClick(myInput)">获取输入框元素</button>
+        <!-- 输出 <input type="text"> -->
+      </div>
+      <br>
+      <div>
+        <input #myInput type="text" (keydown)="onEnter($event, myInput.value)">获取按下键盘事件
+        <button (click)="eventOnClick($event, myInput.value)">获取鼠标点击事件</button>
+        <!-- 输出 <input type="text"> -->
+      </div>
+      <br> 
+    </div>
   `,
   styleUrls: ['./simple-form.component.css']
 })
 export class SimpleFormComponent implements OnInit {
+
+  onClick(value) {
+    console.log(value);
+  }
+
+  eventOnClick(event, value) {
+    console.log(event);
+    console.log(value);
+  }
+
+  onEnter(event, value) {
+    console.log(event);
+    console.log(value);
+  }
 
   constructor() { }
 
