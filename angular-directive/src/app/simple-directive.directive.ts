912d5a76840482398b1505d19817230f133d6e17
XXB <xxblcw@163.com>
2018-06-10 11:09:37 +0800

    事件处理

--- angular-directive/src/app/simple-directive.directive.ts
+++ angular-directive/src/app/simple-directive.directive.ts
@@ -1,19 +1,28 @@
-import { Directive, HostBinding, Input } from '@angular/core';
-// import { getRenderedText } from '@angular/core/src/render3';
+import { Directive, HostBinding, Input, HostListener } from '@angular/core';
+import { getRenderedText } from '@angular/core/src/render3';
 
-// 使用 HostBinding 装饰器，实现元素的属性绑定
-// 本例中该用于演示如何利用 HostBinding 装饰器，设置元素的 innerText 属性
 
 @Directive({
   selector: '[simpleDirective]'
 })
 export class SimpleDirectiveDirective {
 
+  // 利用 Input 装饰器，定义指令的输入属性，从而实现让用户自定义问候内容
   @Input() greet: string;
+
+  // 使用 HostBinding 装饰器，实现元素的属性绑定，本利绑定的元素的innerText 属性
   // @HostBinding() innerText = '这里是simple directive修改过的文档内容';
   @HostBinding() get innerText() {
     return this.greet;
   }
+
+  // 使用 HostListener 属性装饰器，实现元素的事件绑定
+  @HostListener('click', ['$event']) onClick(event) {
+    this.greet = 'HostListener绑定Clicked!事件';
+  }
+
+  // 通过 Attribute 装饰器来获取指令宿主元素的属性值
+
 
   constructor() { }
 
